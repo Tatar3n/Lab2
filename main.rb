ee3dce7f2e4fea1d48606bd52a9705807d7a46df
Tatar3n <97883444+Tatar3n@users.noreply.github.com>
2024-05-07 10:05:09 +0000

    Add Tests

--- main.rb
+++ main.rb
@@ -32,3 +32,28 @@ print(max_pair([-6,-5,3,4]))
 print(mult_complex(1,2,3,4))
 print(array_prime(10))
 print(basket(15, 7))
+
+require 'test/unit'
+
+class Tests < Test::Unit::TestCase
+  def TestFirst
+    assert_equal max_pair([-6,-5,3,4]), 30
+    assert_equal max_pair([1,2,3,4]), 12
+    assert_equal max_pair([-6,-5,3,-7]), 42
+  end
+  def TestSecond
+    assert_equal mult_complex(1,2,3,4), [-5, 10]
+    assert_equal mult_complex(-1,2,5,4), [-13, 6]
+    assert_equal mult_complex(10,2,3,5), [20, 56]
+  end
+  def TestThird
+    assert_equal array_prime(1), [2]
+    assert_equal array_prime(2), [2,3]
+    assert_equal array_prime(3), [2,3,5]
+  end
+  def TestFourth
+    assert_equal basket(15,7), 3
+    assert_equal basket(0.25, 1), 2
+    assert_equal basket(30,30), nil
+  end
+end
